9f397a851ae4c6a3d67021bf2f9bad6ddbb78304
Delavaud Paul-Emile <paul-emile.delavaud@student-cs.fr>
2023-11-16 09:56:08 +0100

    Légère progression dans l'écriture

--- graphique.py
+++ graphique.py
@@ -5,11 +5,23 @@ from matplotlib.animation import FuncAnimation
 # --------------------------------------FONCTIONNALITE 2--------------------------------------
 # --------------------------------------------------------------------------------------------
 
-def trace():
+def trace(Pool):
     """Fonction générant le billard animé"""
     # Pour fermer des plots potentiellement existants
     plt.close()
     # Initialisation de la figure contenant l'animation
     fig = plt.figure("Billard Interactif Techniquement Exploitable")
-    # On récupère les caractéristiques des objets à afficher
+    # Valeurs récupérées 
     
+    def init():
+        """Fonction initialisant l'affichage"""
+
+    def update(frame):
+        """Fonction générant une image de l'animation"""
+    
+    ani = FuncAnimation(fig, update, init_func=init, interval=1000/60, blit=True, repeat=False)
+    plt.show()
+
+
+
+
