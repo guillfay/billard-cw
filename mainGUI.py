ff149df89b874a3ffdd61822673dc00d82a6f338
Delavaud Paul-Emile <paul-emile.delavaud@student-cs.fr>
2023-11-20 12:40:38 +0100

    Ajout LabelFrame

--- mainGUI.py
+++ mainGUI.py
@@ -50,6 +50,7 @@ class InputFrame(ttk.Frame):
         self.rowconfigure(6, weight=1)
         self.rowconfigure(7, weight=1)
         self.rowconfigure(8, weight=1)
+        self.rowconfigure(9, weight=1)
 
         # Enregistrement des fonctions de màj
         self.update_billard = update_billard
@@ -58,31 +59,45 @@ class InputFrame(ttk.Frame):
 
     def __create_widgets(self, valider, billard):
         # Création des labels descriptifs des entrées
-        ttk.Label(self, text="Type de billard choisi :").grid(column=0, row=0)
-        ttk.Label(self, text="Paramètres de frappe :").grid(column=0, row=5)
-        ttk.Label(self, text="Angle de frappe :").grid(column=0, row=6)
-        ttk.Label(self, text="Force de frappe :").grid(column=0, row=7)
+        frame1 = ttk.Labelframe(self, text="Paramètres du billard")
+        frame1.grid(column=0, row=0, columnspan=2, sticky="WE")
+        frame2 = ttk.Labelframe(self, text="Paramètres de frappe")
+        frame2.grid(column=0, row=7, columnspan=2, sticky="WE")
+        ttk.Label(frame1, text="Type de billard choisi :").grid(column=0, row=0)
+        ttk.Label(frame1, text="Masse des boules :").grid(column=0, row=4)
+        ttk.Label(frame1, text="Rayon des boules :").grid(column=0, row=5)
+        ttk.Label(frame1, text="Longueur/largeur de la table :").grid(column=0, row=6)
+        ttk.Label(frame2, text="Angle de frappe :").grid(column=0, row=7)
+        ttk.Label(frame2, text="Force de frappe :").grid(column=0, row=8)
 
         # Création des entrées
         self.choix = tk.IntVar()
         self.choix.set(1)
-        self.choix1_entry = ttk.Radiobutton(self, text="Français", variable=self.choix, value=1)
-        self.choix1_entry.grid(column=1, row=1)
-        self.choix2_entry = ttk.Radiobutton(self, text="Américain", variable=self.choix, value=2)
-        self.choix2_entry.grid(column=1, row=2)
-        self.choix3_entry = ttk.Radiobutton(self, text="Anglais", variable=self.choix, value=3)
-        self.choix3_entry.grid(column=1, row=3)
-        self.choix4_entry = ttk.Radiobutton(self, text="Personnalisé", variable=self.choix, value=4)
-        self.choix4_entry.grid(column=1, row=4)
 
-        self.angle_entry = tk.Scale(self, from_=-180, to=180, orient="horizontal", length=150, tickinterval=90, resolution=1)
-        self.angle_entry.grid(column=1, row=6)
+        self.choix1_entry = ttk.Radiobutton(frame1, text="Français", variable=self.choix, value=1)
+        self.choix1_entry.grid(column=1, row=0)
+        self.choix2_entry = ttk.Radiobutton(frame1, text="Américain", variable=self.choix, value=2)
+        self.choix2_entry.grid(column=1, row=1)
+        self.choix3_entry = ttk.Radiobutton(frame1, text="Anglais", variable=self.choix, value=3)
+        self.choix3_entry.grid(column=1, row=2)
+        self.choix4_entry = ttk.Radiobutton(frame1, text="Personnalisé", variable=self.choix, value=4)
+        self.choix4_entry.grid(column=1, row=3)
+        self.masse_entry = ttk.Entry(frame1)
+        self.masse_entry.grid(column=1, row=4)
+        self.rayon_entry = ttk.Entry(frame1)
+        self.rayon_entry.grid(column=1, row=5)
+        self.length_entry = ttk.Entry(frame1, text="Longueur :")
+        self.length_entry.grid(column=1, row=6)
+        self.width_entry = ttk.Entry(frame1, text="Largeur")
+        self.width_entry.grid(column=2, row=6)
 
-        self.force_entry = tk.Scale(self, from_=0, to=100, orient="horizontal", length=150, tickinterval=25, resolution=1)
-        self.force_entry.grid(column=1, row=7)
-
+        self.angle_entry = tk.Scale(frame2, from_=-180, to=180, orient="horizontal", length=150, tickinterval=90, resolution=1)
+        self.angle_entry.grid(column=1, row=7)
+        self.force_entry = tk.Scale(frame2, from_=0, to=100, orient="horizontal", length=150, tickinterval=25, resolution=1)
+        self.force_entry.grid(column=1, row=8)
+        
         self.validate_button = tk.Button(self, text="Tirer", activebackground="green", fg="green", command=valider)
-        self.validate_button.grid(column=1, row=8)
+        self.validate_button.grid(column=1, row=9)
         
     def valider(self):
         """Récupération des valeurs et mise à jour de l'objet pendule"""
