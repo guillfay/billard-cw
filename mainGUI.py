293e9381ee74934b0726e89658997ba33bd9bc78
Delavaud Paul-Emile <paul-emile.delavaud@student-cs.fr>
2023-11-23 21:50:28 +0100

    Élargissement barre angle + supression choix masse

--- mainGUI.py
+++ mainGUI.py
@@ -55,7 +55,6 @@ class InputFrame(ttk.Frame):
         self.rowconfigure(4, weight=1)
         self.rowconfigure(5, weight=1)
         self.rowconfigure(6, weight=1)
-        self.rowconfigure(7, weight=1)
 
         # Enregistrement des fonctions de màj
         self.change_pool_func = change_pool_func
@@ -71,9 +70,8 @@ class InputFrame(ttk.Frame):
         frame2.grid(column=0, row=4, columnspan=2, sticky="WE")
         # Création des labels descriptifs des entrées
         ttk.Label(frame1, text="Type de billard choisi :").grid(column=0, row=0)
-        ttk.Label(frame1, text="Masse des boules (en kg) :").grid(column=0, row=3)
-        ttk.Label(frame2, text="Angle de frappe (en °) :").grid(column=0, row=5)
-        ttk.Label(frame2, text="Force de frappe (en %) :").grid(column=0, row=6)
+        ttk.Label(frame2, text="Angle de frappe (en °) :").grid(column=0, row=4)
+        ttk.Label(frame2, text="Force de frappe (en %) :").grid(column=0, row=5)
 
         # Création des entrées
         self.choix = tk.IntVar()
@@ -81,25 +79,22 @@ class InputFrame(ttk.Frame):
         self.balls = billard.balls
 
         self.choix1_entry = ttk.Radiobutton(frame1, text="Français", variable=self.choix, value=1)
-        self.choix1_entry.grid(column=1, row=0)
+        self.choix1_entry.grid(column=0, row=0)
         self.choix2_entry = ttk.Radiobutton(frame1, text="Américain", variable=self.choix, value=2)
-        self.choix2_entry.grid(column=1, row=1)
+        self.choix2_entry.grid(column=0, row=1)
         self.choix3_entry = ttk.Radiobutton(frame1, text="Anglais", variable=self.choix, value=3)
-        self.choix3_entry.grid(column=1, row=2)
-        self.masse_entry = ttk.Entry(frame1)
-        self.masse_entry.grid(column=1, row=3)
-        self.masse_entry.insert(0, self.balls[0].mass)
+        self.choix3_entry.grid(column=0, row=2)
         self.validate_button = tk.Button(frame1, text="Valider paramètres", activebackground="green", fg="green",
                                          command=valider)
-        self.validate_button.grid(column=0, row=4, columnspan=2)
+        self.validate_button.grid(column=0, row=3)
 
-        self.angle_entry = tk.Scale(frame2, from_=-180, to=180, orient="horizontal", length=180, tickinterval=90, command=self.angle_test)
-        self.angle_entry.grid(column=1, row=5)
-        self.force_entry = tk.Scale(frame2, from_=0, to=100, orient="horizontal", length=180, tickinterval=25,
+        self.angle_entry = tk.Scale(frame2, from_=-180, to=180, orient="horizontal", length=360, tickinterval=90, command=self.angle_test)
+        self.angle_entry.grid(column=1, row=4)
+        self.force_entry = tk.Scale(frame2, from_=0, to=100, orient="horizontal", length=360, tickinterval=25,
                                     resolution=1)
-        self.force_entry.grid(column=1, row=6)
+        self.force_entry.grid(column=1, row=5)
         self.validate_button = tk.Button(frame2, text="Tirer", activebackground="green", fg="green", command=self.tirer)
-        self.validate_button.grid(column=0, row=7, columnspan=2)
+        self.validate_button.grid(column=0, row=6, columnspan=2)
         
         self.angle = float(self.angle_entry.get())
 
@@ -133,7 +128,7 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Simulation billard')
-        self.geometry('1000x820')
+        self.geometry('1200x820')
         self.protocol("WM_DELETE_WINDOW", self.quit_me)
 
         # Layout de la fenêtre
@@ -150,7 +145,7 @@ class App(tk.Tk):
     def __create_widgets(self):
         """Création de la partie graphe
         Pour l'affichage graphique, on crée une fonction partial qui sera appelée sans paramètre dans GraphFrame"""
-        partial_update_pool = partial(update_pool, self.billard, 1 / 144)
+        partial_update_pool = partial(update_pool, self.billard, 1 / 360)
         self.grap_frame = GraphFrame(self, self.billard, partial_update_pool, self.queue)
         self.grap_frame.grid(column=0, row=0)
         # Création de la partie configuration
@@ -161,7 +156,7 @@ class App(tk.Tk):
         """Fonction pour recréer le billard lorsque l'utilisateur change le type de billard"""
         self.billard = billard
         self.queue = queue
-        partial_update_pool = partial(update_pool, self.billard, 1 / 144)
+        partial_update_pool = partial(update_pool, self.billard, 1 / 360)
         self.grap_frame.draw_canvas(self.billard, partial_update_pool, self.queue)
 
     def tirer(self, energie):
